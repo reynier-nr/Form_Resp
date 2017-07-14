a3fb5ad418b7f8dedc073a9e1a33c6d9d6f5942c
@ReynierNR <reynier@Reynier.local>
2017-07-14 16:30:13 -0700

    $tel

--- enviar.php
+++ enviar.php
@@ -1,12 +1,12 @@
 <?php 
 $destino = "reynier.nr@gmail.com";
 $subject = "www.elyfotografia.com.mx";
-$nombre = $_POST["quiero"];
+$quiero = $_POST["quiero"];
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $tel = $_POST["tel"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Mensaje_desde" . $subject ."\nQuiero: " . $quiero . "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . tel . "\nMensaje: " . $mensaje;
+$contenido = "Mensaje_desde" . $subject ."\nQuiero: " . $quiero . "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . $tel . "\nMensaje: " . $mensaje;
 mail($destino,"Contacto", $contenido);
 header("Location:enviado.html");
 
