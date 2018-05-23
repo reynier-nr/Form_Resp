1393163a04e26738e7e9c623c17c9c5774e440bb
@ReynierNR <reynier@Reynier.local>
2018-05-23 12:13:51 -0700

    ok
    ok

--- enviar.php
+++ enviar.php
@@ -6,8 +6,7 @@ $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $tel = $_POST["tel"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Mensaje_desde" . $subject ."\nQuiero: " . $quiero . "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . $tel . "\nMensaje: " . $mensaje;
-mail($destino,"Contacto", $contenido);
+$contenido = "Mensaje_desde:" . $subject ."\nQuiero: " . $quiero . "\nNombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . $tel . "\nMensaje: " . $mensaje;
+mail($destino,"ClienteWeb", $contenido);
 header("Location:enviado.html");
-
 ?>
